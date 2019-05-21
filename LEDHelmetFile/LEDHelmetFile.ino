45ec72291edf497b59f4fb7b4ff605daff07cdd4
Tobi <spacehomer@live.co.uk>
2019-05-21 15:51:30 +0100

    Added so I can read the values
    Just need to add that it assigns it to the struct then i'll have to add it to so the leds read the data

--- LEDHelmetFile/LEDHelmetFile.ino
+++ LEDHelmetFile/LEDHelmetFile.ino
@@ -1,61 +1,87 @@
 #include <SD.h>
 #include <SPI.h>
+//#include <FastLED.h>
 
-
-#define LEDSize 10
-
+#define LEDSize 100
 
 int cs = 10;
 File myFile;
-String data;
 String buffers;
-int lines;
-int frames;
 
-typedef struct{
-  int red[LEDSize][LEDSize];
-  int green[LEDSize][LEDSize];
-  int blue[LEDSize][LEDSize];
-  boolean selectedLEDs[LEDSize][LEDSize];
+typedef struct frame {
+  byte red[LEDSize];
+  byte green[LEDSize];
+  byte blue[LEDSize];
+  byte selectedLEDs[LEDSize];
   int timeDelay;
-}frame;
+} frame;
 
-void assignValues(File& myFile, int frameSize){
+void assignValues(File& myFile, int frameSize) {
   int listLength = 1;
   int currentFrame = -1;
-  int curChar;
   frame Frames[frameSize];
-  char delim[] = "()";
-  char subDelim = ",";
-  
-    while(myFile.available()){
-      buffers = myFile.readStringUntil('\n');
-      char *ptr = strtok(buffers.c_str(), delim);
-      for(int i = 0; ptr[i] != '\0'; i++){
-        if(ptr[i] == '['){
-          currentFrame++;
-          Serial.println(ptr[i+1]);
-        }else{
-        for(int n = 0; n < 3; n++){
-            if(i == 0){
-              Serial.println(ptr);
-              ptr = strtok(NULL, delim);
-            }else if(i >= 1 ){
-             
+  char delim[] = "() ";
+  while (myFile.available()) {
+    buffers = myFile.readStringUntil('\n');
+    char *ptr = strtok(buffers.c_str(), delim);
+    for (int i = 0; ptr[i] != '\0'; i++) {
+      if (ptr[i] == '[') {
+        currentFrame++;
+        Serial.println(ptr[i+1]);
+        ptr = strtok(NULL, delim);
+        Serial.println(i);
+        incriment = incriment + 1;
+      } else {
+          Serial.println(i);
+          int r,g,b;
+          char val[1];
+          
+          if(sscanf(ptr, "%[^,],%d,%d,%d", &val, &r, &g, &b) != 1){
+         
+            Frames[currentFrame].red[incriment] = r;
+            Frames[currentFrame].green[incriment] = g;
+            Frames[currentFrame].blue[incriment] = b;
+            if(val[0] == 't'){
+              Frames[currentFrame].selectedLEDs[incriment] = 1;
+            }else{
+              Frames[currentFrame].selectedLEDs[incriment] = 0;
+            }
+           
           }
+          
+          Serial.println(Frames[currentFrame].red[incriment]);
+          Serial.println(Frames[currentFrame].green[incriment]);
+          Serial.println(Frames[currentFrame].blue[incriment]);
+          Serial.println(Frames[currentFrame].selectedLEDs[incriment]);
+          
+          //Serial.println(ptr);
+          ptr = strtok(NULL, delim);
+          
         }
       }
     }
   }
-}
 
-int readFrames(File& myFile, int& frameAmount){
+
+
+
+
+  
+  /*for (int c = 0; c < LEDSize; c++) {
+    if (Frames[0].selectedLEDs[c] == 1) {
+      Serial.println("True");
+    } else {
+      Serial.println("False");
+    }
+  }*/
+
+void readFrames(File& myFile, int& frameAmount) {
   String tempBuffer;
   tempBuffer = myFile.readStringUntil('>');
   char delim[] = "<>";
-  char* line = strtok(tempBuffer.c_str(),delim );
-  while(myFile.available()){ 
-    while(line != NULL){
+  char* line = strtok(tempBuffer.c_str(), delim );
+  while (myFile.available()) {
+    while (line != NULL) {
       frameAmount = atoi(line);
       Serial.println(frameAmount);
       break;
@@ -64,30 +90,28 @@ int readFrames(File& myFile, int& frameAmount){
   }
 }
 void setup() {
-  // put your setup code here, to run once:
   char myFileName[] = "NEW.txt";
 
   Serial.begin(9600);
-  while(!Serial){
-    
+  while (!Serial) {
+
   }
   Serial.println("Connecting to SD card...");
   pinMode(cs, OUTPUT);
   pinMode(SS, OUTPUT);
-  if(!SD.begin(cs)){
+  if (!SD.begin(cs)) {
     Serial.println("SD did not initalize");
   }
   myFile = SD.open(myFileName, FILE_READ);
-  if(!myFile){
+  if (!myFile) {
     Serial.println("Could not open file");
-    while(1);
+    while (1);
   }
-  //char temp[10];
+
   int frameAmount;
   readFrames(myFile, frameAmount);
-  
-  assignValues(myFile,frameAmount);
-  
+  assignValues(myFile, frameAmount);
+
 }
 
 void loop() {
